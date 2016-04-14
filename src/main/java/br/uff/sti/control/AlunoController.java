f64f6fbb8cf10573584e60c12115c156a2b833bf
uff <uff@sti02>
2016-04-14 17:55:04 -0300

    interface criada

--- src/main/java/br/uff/sti/control/AlunoController.java
+++ src/main/java/br/uff/sti/control/AlunoController.java
@@ -7,17 +7,22 @@ package br.uff.sti.control;
 
 import br.uff.sti.model.domain.Aluno;
 import br.uff.sti.model.service.AlunoService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
  *
  * @author uff
  */
-@RestController
+//@RestController
+@Controller
 @RequestMapping(value = "/aluno")
 public class AlunoController {
 
@@ -25,56 +30,84 @@ public class AlunoController {
     private AlunoService alunoService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    ResponseEntity<Iterable<Aluno>> todos() {
-        return new ResponseEntity<>(alunoService.todos(), HttpStatus.OK);
+    String todos(Model model) {
+       model.addAttribute("alunos", alunoService.todos());
+       model.addAttribute("view", "fragments/exibeAlunosContent");
+       return "index";
     }
-
-    @RequestMapping(value = "{matricula}", method = RequestMethod.GET)
-    ResponseEntity<Aluno> buscar(
-            @PathVariable String matricula
-    ) {
+    
+    @RequestMapping(value = "/{matricula}", method = RequestMethod.GET)
+    String buscar(@PathVariable String matricula,Model model) {
         Aluno aluno = alunoService.busca(matricula);
-        if (aluno == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(aluno, HttpStatus.OK);
+        List<Aluno> alunos = new ArrayList<>();
+        alunos.add(aluno);
+        model.addAttribute("alunos", alunos);
+        model.addAttribute("view", "fragments/exibeAlunosContent");
+        return "index";
     }
-
-    @RequestMapping(value = "novo/", method = RequestMethod.GET)
-    ResponseEntity<Aluno> criarGet(
-            @RequestParam(value = "matricula", required = true) String matricula,
-            @RequestParam(value = "nome", required = true) String nome,
-            @RequestParam(value = "curso", required = true) String codCurso
-    ) {
-        Aluno aluno = alunoService.salva(alunoService.cria(matricula, nome, codCurso));
-        if(aluno==null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(aluno, HttpStatus.CREATED);
+    @RequestMapping(value = "busca/", method = RequestMethod.POST)
+    String buscarPost(@RequestParam(value = "matricula", required = true) String matricula,Model model) {
+        Aluno aluno = alunoService.busca(matricula);
+        List<Aluno> alunos = new ArrayList<>();
+        alunos.add(aluno);
+        model.addAttribute("alunos", alunos);
+        model.addAttribute("view", "fragments/exibeAlunosContent");
+        return "index";
     }
-
+//
+    @RequestMapping(value = "cadastra/", method = RequestMethod.GET)
+    String criarPost( Model model) {
+        
+        model.addAttribute("view", "fragments/criaAlunoContent");
+        return "index";
+    }
+    
     @RequestMapping(value = "novo/", method = RequestMethod.POST)
-    ResponseEntity<Aluno> criarPost(
+    String criarPost(
             @RequestParam(value = "matricula", required = true) String matricula,
             @RequestParam(value = "nome", required = true) String nome,
-            @RequestParam(value = "curso", required = true) String codCurso
+            @RequestParam(value = "curso", required = true) String codCurso,
+            Model model
     ) {
         Aluno aluno = alunoService.salva(alunoService.cria(matricula, nome, codCurso));
-        if(aluno==null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(aluno, HttpStatus.CREATED);
+         List<Aluno> alunos = new ArrayList<>();
+        alunos.add(aluno);
+        model.addAttribute("alunos", alunos);
+        model.addAttribute("view", "fragments/exibeAlunosContent");
+        return "index";
     }
+    
+    
+    
+//    @RequestMapping(value = "", method = RequestMethod.GET)
+//    ResponseEntity<Iterable<Aluno>> todos() {
+//        return new ResponseEntity<>(alunoService.todos(), HttpStatus.OK);
+//    }
+//
+//    @RequestMapping(value = "{matricula}", method = RequestMethod.GET)
+//    ResponseEntity<Aluno> buscar(
+//            @PathVariable String matricula
+//    ) {
+//        Aluno aluno = alunoService.busca(matricula);
+//        if (aluno == null) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<>(aluno, HttpStatus.OK);
+//    }
+//
+//    @RequestMapping(value = "novo/", method = RequestMethod.POST)
+//    ResponseEntity<Aluno> criarPost(
+//            @RequestParam(value = "matricula", required = true) String matricula,
+//            @RequestParam(value = "nome", required = true) String nome,
+//            @RequestParam(value = "curso", required = true) String codCurso
+//    ) {
+//        Aluno aluno = alunoService.salva(alunoService.cria(matricula, nome, codCurso));
+//        if(aluno==null){
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//        return new ResponseEntity<>(aluno, HttpStatus.CREATED);
+//    }
 
-    @RequestMapping(value = "novo/", method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Aluno> criarPorObjeto(
-            @RequestBody Aluno aluno
-    ) {
-        aluno = alunoService.salva(aluno);
-        return new ResponseEntity<>(aluno, HttpStatus.CREATED);
-    }
     //Requestbody!=model atribute
     //exception handler spring
 }
